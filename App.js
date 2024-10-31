50c9bfb6bd5d11e707ef52dca4a59b81e54f7875
Gustavo Goncalves Dos Santos <gustavo.330123@alunosatc.edu.br>
2024-10-30 21:26:00 -0300

    feito pesquisa de filmes funcional!

--- App.js
+++ App.js
@@ -1,39 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const API_KEY = '1CD66749';
 
 export default function App() {
 
-  const searchMovies = async () => {
-      try {
-          const result  = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=spider`);
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
-          const data = await result.json();
-          console.log(data);
+    const searchMovies = async () => {
+        try {
+            const result  = await fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`);
 
-      } catch(error) {
-          throw error;
-      }
-  }
+            const data = await result.json();
+            setMovies(data.Search)
 
-  return (
-    <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <StatusBar style="auto" />
-        <Button 
-            title='Buscar Filmes'
-            onPress={searchMovies}
-        />
-    </View>
-  );
+        } catch(error) {
+            throw error;
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Buscar de Filmes!</Text>
+            <StatusBar style="auto" />
+            <TextInput 
+                placeholder='Digite aqui para pesquisar...'
+                value={query}
+                onChangeText={setQuery}
+            />
+            <Button 
+                title='Buscar Filmes'
+                onPress={searchMovies}
+            />
+            <FlatList 
+                data={movies}
+                keyExtractor={(item) => item.imdbID}
+                renderItem={({ item }) => <Text>{ item.Title }</Text>}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 300
+    }
 });
